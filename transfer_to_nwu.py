83921852a136c2f4d20fd1fa9743eb63dac2b0f4
Jia Fu Low <jia.fu.low@cern.ch>
2015-09-04 08:53:22 -0500

    Rewrote transfer_to_nwu.py

--- transfer_to_nwu.py
+++ transfer_to_nwu.py
@@ -4,8 +4,11 @@ import os
 import re
 
 
-old_dir = "/eos/uscms/store/user/jiafu/HZG/nuTuples_v9.10_8TeV/MC/TTToHCWB_HToMuMu_M-30_8TeV_pythia8175/"
-new_dir = "/tthome/share/noobs/nuTuples_v9.10_8TeV/MC/TT_FCNH_M30/"
+#old_dir = "/eos/uscms/store/user/jiafu/HZG/nuTuples_v9.10_8TeV/MC/TTToHCWB_HToMuMu_M-30_8TeV_pythia8175/"
+#new_dir = "/tthome/share/noobs/nuTuples_v9.10_8TeV/MC/TT_FCNH_M30/"
+
+old_dir = "/uscms_data/d2/jiafu/HZG/CMSSW_6_1_1/src/HZG_Analyzer/HiggsZGAnalyzer/skims/nuTuples_v9.10_8TeV/MC/"
+new_dir = "/tthome/jiafulow/HZG/CMSSW_6_1_1/src/HZG_Analyzer/HiggsZGAnalyzer/skims/nuTuples_v9.10_8TeV/MC/"
 
 remote_host = "jiafulow@ttgrid01.ci.northwestern.edu"
 
@@ -35,34 +38,32 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 # ______________________________________________________________________________
+datasets = []
+
 def check():
-    files = os.listdir(old_dir)
-    
-    if len(files) == 0:
-        raise Exception("No file is found.")
-    
-    print "There are %i files." % (len(files))
-    
-    files = sorted(files, key=natural_sort_key)
-    
-    last_file = files[-1]
-    last_file_s = split(last_file)
-    
-    if last_file_s[0] != "nuTuple_" or not last_file_s[1].isdigit():
-        raise Exception("File name is not matched.")
-    
-    print "There should be %i files." % (int(last_file_s[1]))
-    
-    if not len(files) == int(last_file_s[1]):
-        raise Exception("Some files are missing.")
-        
-    print "All the files are there."
+    global datasets
+    datasets = os.listdir(old_dir)
+    datasets = filter(lambda x: os.path.isdir(os.path.join(old_dir, x)), datasets)
+
+    if len(datasets) == 0:
+        raise Exception("No dataset is found.")
+
+    print "There are %i datasets." % (len(datasets))
+
+    datasets = sorted(datasets, key=natural_sort_key)
+
+    print "-" * 20
+    for dataset in datasets:
+        print dataset
     
 
-def transfer():
+def transfer(rsync=True):
     print bcolors.BOLD + "\n>>> Transfer <<<" + bcolors.ENDC
-    print 'ssh -Y %s "mkdir -p %s"' % (remote_host, new_dir)
-    print 'scp -pr %s %s:%s' % (old_dir, remote_host, new_dir)
+    if not rsync:
+        print 'ssh -Y %s "mkdir -p %s"' % (remote_host, new_dir)
+        print 'scp -pr %s %s:%s' % (old_dir, remote_host, new_dir)
+    else:
+        print 'rsync -avz %s %s:%s' % (old_dir, remote_host, new_dir)
 
 
 # ______________________________________________________________________________
