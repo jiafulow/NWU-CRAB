082fb7a5c561a1d615d3e68372cf921bdc8bb8c2
Jia Fu Low <jia.fu.low@cern.ch>
2015-09-04 08:52:39 -0500

    Minor modifications

--- status_get_status.sh
+++ status_get_status.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-DIR=MC1/
+DIR=MC2/
 multicrab -status -c ${DIR}
 multicrab -get -c ${DIR}
 multicrab -status -c ${DIR}
+
+#DIR=MC1/ZZ/
+#crab -status -c ${DIR}
+#crab -get -c ${DIR}
+#crab -status -c ${DIR}
--- submit_submit_submit.sh
+++ submit_submit_submit.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-DIR=MC1/ZZ/
-crab -submit 500 -c ${DIR}
-crab -submit 500 -c ${DIR}
-crab -submit 500 -c ${DIR}
-crab -submit 500 -c ${DIR}
-crab -submit 500 -c ${DIR}
+DIR=MC2
+multicrab -submit 500 -c ${DIR}
+multicrab -submit 500 -c ${DIR}
+multicrab -submit 500 -c ${DIR}
+
+#DIR=MC1/ZZ/
+#crab -submit 500 -c ${DIR}
+#crab -submit 500 -c ${DIR}
+#crab -submit 500 -c ${DIR}
+#crab -submit 500 -c ${DIR}
+#crab -submit 500 -c ${DIR}
